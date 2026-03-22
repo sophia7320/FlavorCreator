9127ead0b8aa6356908008daf48904f8b8b7fb9b
Sophia7320 <2060205344@qq.com>
2026-03-22 17:12:15 +0800

    加入文档

--- backend/src/main/java/flcr/backend/BackendApplication.java
+++ backend/src/main/java/flcr/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package flcr.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BackendApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/flcr/backend/admin/AdminController.java
@@ -0,0 +1,7 @@
+package flcr.backend.admin;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+}
