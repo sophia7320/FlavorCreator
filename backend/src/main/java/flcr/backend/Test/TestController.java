39eb7c45454d591f06fb109bd8c377fa71b0ddc2
Sophia7320 <2060205344@qq.com>
2026-03-22 17:42:27 +0800

    test

--- backend/src/main/java/flcr/backend/Test/TestController.java
+++ backend/src/main/java/flcr/backend/Test/TestController.java
@@ -1,6 +1,5 @@
 package flcr.backend.Test;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
