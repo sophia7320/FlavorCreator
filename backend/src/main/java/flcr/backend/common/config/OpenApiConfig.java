d6b318c4840ff0bdce6f7729886c29e3529d21e3
Sophia7320 <2060205344@qq.com>
2026-04-29 00:52:32 +0800

    feat: 配置 Swagger/OpenAPI 接口文档自动生成
    - 新增 springdoc-openapi 2.7.0 依赖
    - OpenApiConfig: 标题 创味机 API，JWT Bearer 鉴权
    - Swagger UI: /swagger-ui.html，API JSON: /v3/api-docs
    - 生产环境关闭 api-docs 保证安全
    - 无需改动 Controller，自动解析注解生成文档

---
+++ backend/src/main/java/flcr/backend/common/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package flcr.backend.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("创味机 API")
+                        .version("1.0.0")
+                        .description("FlavorCreator 微信小程序后端接口文档"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
