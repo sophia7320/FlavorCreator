5eb79dfb31eb6c36f09bb3b3572cc6984438ecc5
NatalieLeeeeee <349938537@qq.com>
2026-05-29 15:48:30 +0800

    Update Recipe_Request.java

--- backend/src/main/java/flcr/backend/recipe/DTO/request/Recipe_Request.java
+++ backend/src/main/java/flcr/backend/recipe/DTO/request/Recipe_Request.java
@@ -36,9 +36,6 @@ public class Recipe_Request {
         @Schema(description = "口味要求", example = "[\"清淡\", \"少油\"]")
         private List<String> taste;
         
-        @Schema(description = "饮食禁忌/要求", example = "[\"低卡\"]")
-        private List<String> dietary;
-        
         @Schema(description = "期望烹饪时长（分钟）", example = "30")
         private Integer cookTime;
         
