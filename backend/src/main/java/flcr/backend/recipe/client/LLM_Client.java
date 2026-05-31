d8591c4338d942d5a9e5f59ed868a7dcd8a00188
NatalieLeeeeee <349938537@qq.com>
2026-05-31 12:01:29 +0800

    Update LLM_Client.java

--- backend/src/main/java/flcr/backend/recipe/client/LLM_Client.java
+++ backend/src/main/java/flcr/backend/recipe/client/LLM_Client.java
@@ -11,13 +11,13 @@ import java.util.Map;
 @Component
 public class LLM_Client {
 
-    @Value(" $ {llm.api.url:https://api.openai.com/v1/chat/completions}")
+    @Value(" $ {https://api.siliconflow.cn/v1/chat/completions}")
     private String apiUrl;
 
-    @Value(" $ {llm.api.model:gpt-3.5-turbo}")
+    @Value(" $ {llm.api.model:Pro/zai-org/GLM-4.7}")
     private String modelName;
 
-    @Value(" $ {llm.api.key:sk-vigcsnmtiayvgrwtudutzqvnqzrluppqppgrrrzryynvkmov}")
+    @Value(" $ {llm.api.key:sk-pwtinglhtvzmzvbgempceozorwujlepqtxarjfslbzsvmrnx}")
     private String apiKey;
 
     private final RestTemplate restTemplate = new RestTemplate(); // Spring 提供的标准 HTTP 请求工具
