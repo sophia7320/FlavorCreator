165188ef73a88f885cc83e051581886c31d0e47e
Frank-Y81 <zhang6692262@163.com>
2026-06-01 22:56:28 +0800

    社区无限下拉刷新
    还有一点关于个人页面的框架

--- miniprogram/pages/community/community.js
+++ miniprogram/pages/community/community.js
@@ -1,4 +1,7 @@
 // pages/community/community.js
+const { API_CONFIG } = require('../../config/api')
+const { request } = require('../../utils/request')
+
 Page({
 
   /**
@@ -24,59 +27,105 @@ Page({
 			{userName: 'User Name', userImg: 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/user.svg', recipeName: 'Addtional info goes here', recipeImage: 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/image.svg'},
 			{userName: 'User Name', userImg: 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/user.svg', recipeName: 'Addtional info goes here', recipeImage: 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/image.svg'},
 		],
+		page: 1,
+		pageSize: 10,
+		loadingStatus: '',
+		isRequesting: false,
+		hasMore: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.loadData(true)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().onTabPageShow()
     }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().onTabPageShow()
     }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide() {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload() {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh() {
-
+    this.loadData(true)
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom() {
+    this.loadData(false)
+  },
 
+  loadData(isRefresh = false) {
+    if (this.data.isRequesting) {
+      return
+    }
+    if (!isRefresh && !this.data.hasMore) {
+      return
+    }
+
+    this.setData({ isRequesting: true, loadingStatus: 'loading' })
+
+    const page = isRefresh ? 1 : this.data.page
+
+    request(API_CONFIG.community.list, { page, pageSize: this.data.pageSize }, { showLoading: false })
+      .then(res => {
+        const list = res.data.list || res.data || []
+        const hasMore = list.length >= this.data.pageSize
+
+        this.appendCards(list, isRefresh)
+
+        this.setData({
+          page: isRefresh ? 2 : page + 1,
+          hasMore,
+          loadingStatus: hasMore ? '' : 'noMore',
+          isRequesting: false
+        })
+      })
+      .catch(() => {
+        this.setData({ loadingStatus: 'error', isRequesting: false })
+      })
+  },
+
+  appendCards(cards, isRefresh = false) {
+    let newLeftList = isRefresh ? [] : [...this.data.leftList]
+    let newRightList = isRefresh ? [] : [...this.data.rightList]
+
+    cards.forEach((item, index) => {
+      const card = {
+        id: item.id || item._id || Date.now() + index,
+        userName: item.userName || item.user_name || 'User Name',
+        userImg: item.userImg || item.user_avatar || 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/user.svg',
+        recipeName: item.recipeName || item.title || 'Addtional info goes here',
+        recipeImage: item.recipeImage || item.image || 'https://miniprogram-img-1422554268.cos.ap-guangzhou.myqcloud.com/icon/community/image.svg'
+      }
+
+      const totalCount = newLeftList.length + newRightList.length
+      if (totalCount % 2 === 0) {
+        newLeftList.push(card)
+      } else {
+        newRightList.push(card)
+      }
+    })
+
+    this.setData({ leftList: newLeftList, rightList: newRightList })
+  },
+
+  onScrollToLower() {
+    this.loadData(false)
   },
 
   /**
