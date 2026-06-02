c9be59175d3d7237421eda595f9668833f854cb8
NatalieLeeeeee <349938537@qq.com>
2026-06-02 20:13:15 +0800

    Update Recipe_Request.java

--- backend/src/main/java/flcr/backend/recipe/DTO/request/Recipe_Request.java
+++ backend/src/main/java/flcr/backend/recipe/DTO/request/Recipe_Request.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
